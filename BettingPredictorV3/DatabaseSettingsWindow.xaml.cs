a230cc1ca8aa598df8634759f9e3c9927ab62228
mustardpower <paul_gothard93@hotmail.co.uk>
2019-12-16 13:37:30 +0000

    apply database settings
    apply settings when OK is clicked

--- BettingPredictorV3/DatabaseSettingsWindow.xaml.cs
+++ BettingPredictorV3/DatabaseSettingsWindow.xaml.cs
@@ -27,6 +27,19 @@ namespace BettingPredictorV3
 
         private void btnOK_Click(object sender, RoutedEventArgs e)
         {
+            List<string> bookmakersSelected = new List<string>();
+
+            if (bet365Checkbox.IsChecked ?? false) { bookmakersSelected.Add("Bet 365"); }
+            if (interwettenCheckbox.IsChecked ?? false) { bookmakersSelected.Add("InterWetten"); }
+            if (betWinCheckbox.IsChecked ?? false) { bookmakersSelected.Add("BetWin"); }
+            if (victorChandlerCheckbox.IsChecked ?? false) { bookmakersSelected.Add("Victor Chandler"); }
+            if (williamHillCheckbox.IsChecked ?? false) { bookmakersSelected.Add("William Hill"); }
+            if (pinnacleSportCheckbox.IsChecked ?? false) { bookmakersSelected.Add("Pinnacle Sport"); }
+
+            DatabaseSettings.BookmakersUsed = bookmakersSelected;
+            DatabaseSettings.PopulateDatabase = populateDatabaseCheckBox.IsChecked ?? false;
+            DatabaseSettings.IgnorePlayedFixtures = ignoredPlayedFixturesCheckbox.IsChecked ?? false;
+
             DialogResult = true;
         }
 
