5b2502c0500ae7278896bad27c9b83cd7600bcef
mustardpower <paul_gothard93@hotmail.co.uk>
2019-12-18 15:42:55 +0000

    add entity framework to project

---
+++ BettingPredictorV3/FootballResultsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using BettingPredictorV3.DataStructures;
+
+namespace BettingPredictorV3
+{
+    public class FootballResultsDbContext : DbContext
+    {
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Fixture> Fixtures { get; set; }
+    }
+}
